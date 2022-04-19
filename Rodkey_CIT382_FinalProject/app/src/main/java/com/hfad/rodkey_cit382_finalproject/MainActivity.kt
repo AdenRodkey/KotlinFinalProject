583a95e89dfd7003fe643dd0426e4db8255a7a76
AdenRodkey <98115135+AdenRodkey@users.noreply.github.com>
2022-04-19 14:39:40 -0400

    Updated code.

--- Rodkey_CIT382_FinalProject/app/src/main/java/com/hfad/rodkey_cit382_finalproject/MainActivity.kt
+++ Rodkey_CIT382_FinalProject/app/src/main/java/com/hfad/rodkey_cit382_finalproject/MainActivity.kt
@@ -2,9 +2,15 @@ package com.hfad.rodkey_cit382_finalproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     val dictionary: MutableList<Word> = mutableListOf()
+    val wordview = findViewById<TextView>(R.id.word)
+    val defview = findViewById<TextView>(R.id.definition)
+    val genbutton = findViewById<Button>(R.id.genbutton)
+    val introview = findViewById<TextView>(R.id.intro)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -12,12 +18,17 @@ class MainActivity : AppCompatActivity() {
 
         addWord("Run", "move at a speed faster than a walk")
         addWord("Run", "move a speed slower than a run")
-
-        for(i in dictionary)
-        {
-            println("Word:" + i.word)
-            println("Definition:" + i.definition)
+        introview.text = "Dictionary.IO"
+        
+        genbutton.setOnClickListener {
+            for(i in dictionary)
+            {
+                wordview.text = i.word
+                defview.text = i.definition
+            }
         }
+
+
     }
 
     fun addWord(word : String, definition : String)
